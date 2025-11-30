973449c39af81b7508596d8003b6d51e35ab768b
Valdenor <vfval09@gmail.com>
2025-11-29 21:10:54 -0300

    first commit: Inicializa Spring Framework

---
+++ src/main/java/br/com/threadstech/Stockfy/StockfyApplication.java
@@ -0,0 +1,13 @@
+package br.com.threadstech.Stockfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockfyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/threadstech/Stockfy/StockfyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.threadstech.Stockfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
