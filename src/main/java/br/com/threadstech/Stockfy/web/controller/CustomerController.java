8b64c848b2186604e7655646618b89c62491b844
Valdenor <vfval09@gmail.com>
2025-12-09 21:36:13 -0300

    security: Adiciona verificações de role para acesso a recursos de customers, employees e payments.

--- src/main/java/br/com/threadstech/Stockfy/web/controller/CustomerController.java
+++ src/main/java/br/com/threadstech/Stockfy/web/controller/CustomerController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// FIXME: Adicionar verificações de role para os endpoints
+// TODO: Adicionar método get para pagamentos pendentes
 
 @Slf4j
 @RestController
