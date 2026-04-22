518c55521380f1f6b56d57fe5a834d7ef87e3dfd
Valdenor <vfval09@gmail.com>
2026-04-22 13:52:26 -0300

    test: add email and password value objects with tests

---
+++ src/main/java/br/com/threadstech/stockfy/modules/users/domain/model/Password.java
@@ -0,0 +1,9 @@
+package br.com.threadstech.stockfy.modules.users.domain.model;
+
+public record Password(String value) {
+    public Password {
+        if (value == null || value.isBlank() || value.length() < 8) {
+            throw new IllegalArgumentException("Password must have at least 8 characters");
+        }
+    }
+}
